265a539de6b103be29c623446568ebd7eb4e18ff
Erangamadhushan <erangamadhushann956@gmail.com>
2024-06-21 04:22:43 +0530

    add new js file it's called animation.js and change .div2 .courese elements

---
+++ animation.js
@@ -0,0 +1,20 @@
+const observer = new IntersectionObserver((entries,observer) => {
+    entries.forEach((entry) => {
+        if(entry.isIntersecting) {
+            entry.target.classList.add('show');
+        }
+        else {
+            entry.target.classList.remove('show');
+        }
+    })
+},{
+    root:null,
+    rootMargin:'100px',
+    threshold:0.6,
+})
+let courses = document.querySelectorAll('.courses');
+window.addEventListener('scroll',() => {
+    courses.forEach((course) => {
+        observer.observe(course);
+    })
+})
