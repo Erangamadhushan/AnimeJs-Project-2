c6103b1f98ccfcaa0a8e15dfdf48f4c7f0e72b4b
Erangamadhushan <erangamadhushann956@gmail.com>
2024-06-21 05:19:10 +0530

    add animation to div3 elements

--- animation.js
+++ animation.js
@@ -13,8 +13,35 @@ const observer = new IntersectionObserver((entries,observer) => {
     threshold:0.6,
 })
 let courses = document.querySelectorAll('.courses');
+let div3_image = document.querySelector('.div3 .div3_image');
+let div3_content = document.querySelector('.div3 .div_content');
+
+const observer1 = new IntersectionObserver(entry => {
+    if(entry.isIntersecting) {
+        entry.target.classList.add('show1');
+    }
+},{
+    root:null,
+    rootMargin:'100px',
+    threshold:0.5,
+});
+const observer2 = new IntersectionObserver(entry => {
+    if(entry.isIntersecting) {
+        entry.target.classList.add('show1');
+    }
+},{
+    root:null,
+    rootMargin:'100px',
+    threshold:0.5
+})
 window.addEventListener('scroll',() => {
     courses.forEach((course) => {
         observer.observe(course);
-    })
+    });
+    
+});
+window.addEventListener('scroll',() => {
+    observer1.observe(div3_image);
+    observer2.observe(div3_content);
 })
+
